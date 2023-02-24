0159588e8ad773b533126cdaf2f4e9486451d983
k971119 <hanyul97@naver.com>
2023-02-24 17:19:40 +0900

    Crawling Naver News HeadLine

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -1,8 +1,32 @@
 package com.test;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
+        //네이버 뉴스
+        String url = "https://news.naver.com/main/list.nhn?mode=LS2D&sid2=263&sid1=101&mid=shm&date=20190612&page=1";
 
+        Document doc;
+        try {
+            doc = Jsoup.connect(url).get();
+        }catch (IOException ie){
+            System.out.println("읽기 에러");
+            return;
+        }
+        Elements elements = doc.select("div[class=\"list_body newsflash_body\"]");
+
+        for(Element e : elements.select("dt")){
+            if(e.className().equals("photo")){
+                continue;
+            }
+            System.out.println(e.text());
+        }
     }
 }
